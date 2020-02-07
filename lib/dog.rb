2770e8307adcbb773ee9e44a9ab22605a6c4b06c
Edward Williams <eddie_w38@yahoo.com>
2020-02-07 03:26:48 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,20 @@
+class Dog 
+  def initialize(name, breed = "Mutt")
+    @name = name
+    @breed = breed
+  end
+
+ 
+ def name=(name, breed)
+   @name
+   @breed
+ end
+ 
+  def name
+    @name
+    @breed
+  end
+  
+ end
+ 
+ 
